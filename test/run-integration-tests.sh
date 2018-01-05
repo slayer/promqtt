80c0e2b9289099509e74bcdae42c189eba3243a0
Vladislav Moskovets <github@vlad.org.ua>
2018-01-05 12:48:34 +0200

    fix travis

--- test/run-integration-tests.sh
+++ test/run-integration-tests.sh
@@ -15,11 +15,12 @@ cleanup() {
 trap cleanup INT TERM
 
 echo "=> Starting exporter"
-./promqtt -config.file config.yaml.dist &
+./promqtt -config.file config.yaml.dist -log.level=debug &
 service_pid=$!
+mosquitto_pub -t user@domain.com/001122334455/dhtt1 -r -m 12.34
 
-echo "=> Waiting 5s"
-sleep 5
+echo "=> Waiting 1s"
+sleep 1
 
 echo "=> Requesting /metrics"
 curl --silent --max-time 2 http://localhost:9214/metrics > test/out.log
@@ -28,4 +29,4 @@ echo "=> Killing exporter (pid=${service_pid})"
 kill $service_pid
 
 echo "=> Checking result"
-grep 'promqtt_mqtt_messages_received_total [[:digit:]]' test/out.log
+grep 'promqtt_dhtt1{device="001122334455",user="user@domain.com"} 12.34' test/out.log
