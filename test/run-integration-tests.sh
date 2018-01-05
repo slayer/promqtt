423cd3a6cf6e724ea86c0bac99c1c7b5ed78a254
Vlad <devvlad@gmail.com>
2018-01-05 12:52:57 +0200

    Travis (#1)
    * travis packages
    
    * update Makefile for deps
    
    * up
    
    * fix travis

--- test/run-integration-tests.sh
+++ test/run-integration-tests.sh
@@ -15,11 +15,12 @@ cleanup() {
 trap cleanup INT TERM
 
 echo "=> Starting exporter"
-./promqtt -config.file config.yaml.dist &
+./promqtt -config.file config.yaml.dist -log.level=debug &
 service_pid=$!
+mosquitto_pub -t user@domain.com/001122334455/dhtt1 -r -m 12.34
 
-echo "=> Waiting 5s"
-sleep 5
+echo "=> Waiting 1s"
+sleep 1
 
 echo "=> Requesting /metrics"
 curl --silent --max-time 2 http://localhost:9214/metrics > test/out.log
@@ -28,5 +29,4 @@ echo "=> Killing exporter (pid=${service_pid})"
 kill $service_pid
 
 echo "=> Checking result"
-grep 'probe_mqtt_started_total{broker="ssl://broker.mqttdashboard.com:8883",name="mqtt broker SSL"} [[:digit:]]' test/out.log
-grep 'probe_mqtt_started_total{broker="tcp://test.mosquitto.org:1883",name="mqtt broker NONSSL"} [[:digit:]]' test/out.log
+grep 'promqtt_dhtt1{device="001122334455",user="user@domain.com"} 12.34' test/out.log
